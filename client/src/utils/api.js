2de1d30a3ca0e1f7bcc8d1640e67493c70ed78a9
Varshith <varshithchinthapatla5@gmail.com>
2026-05-19 15:58:50 +0530

    frontend deployment config

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://explain-like-friend-backend.onrender.com/",
 });
 
 API.interceptors.request.use((config) => {
